8ce0c33ae6f6f1ff3ec02d9aa165bfcedee89eee
Mike Shippy <mikeship04@gmail.com>
2022-06-09 16:26:18 -0700

    commit tree 5bc077c311e9eeb278dc01bc815024d87b1827f2

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+//this function should match drivers case sensitive
+function findMatching(anArray, aString) {
+    return anArray.filter(
+        (driverName) => driverName.toLowerCase() === aString.toLowerCase()
+    );
+}
+
+
+//this function should return drivers names who begin with provided letters
+function fuzzyMatch(anArray, someStrings){
+    return anArray.filter(
+        (driverName) =>
+        driverName.toLowerCase().indexOf(someStrings.toLowerCase()) === 0
+    );
+}
+
+// each driver object has 2 properties name and hometown
+//return each element whose name matches
+function matchName(anArrayOfObjects, aString) {
+    return anArrayOfObjects.filter((get) => get.name === aString);
+}
